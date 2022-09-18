2f4a38f3536eb7c21fac348ed4d2e7a6999821e8
sharon-youn <jos9187@naver.com>
2022-09-18 23:54:10 +0900

    4주차 2562번 추가 수정 완료

--- sharon-yoon(Runjs)/week4/02_2562.js
+++ sharon-yoon(Runjs)/week4/02_2562.js
@@ -1,8 +1,11 @@
 
 const fs = require("fs");
-
+// node.js의 fs 모듈을 사용해 파일을 읽어온다
+// 출력이 다음 줄에 있을땐 split('\n')으로 하고 
+// 옆에 있을떈 split(' ')로 하기!! 
 const input = require('fs').readFileSync('dev/stdin').toString().split('\n').map(Number)
-
+//require('fs').readFileSync('dev/stdin')
+// 예제.txt
 
 // 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 
@@ -11,11 +14,9 @@ const input = require('fs').readFileSync('dev/stdin').toString().split('\n').map
 let max = Math.max(...input)
 
 let index;
-
+//  식을 더 짧게하고 싶었으나 실패ㅠㅠㅠ 
 for(let i = 0; i<input.length; i++){
         // i는 0부터 시작하니까 순서 구하려면 +1 해주기
-   if(input[i] === max ) {
-    index = i+1 
-   }
+   input[i] === max ? index = i+1 : null;
 }
 console.log(max, index)

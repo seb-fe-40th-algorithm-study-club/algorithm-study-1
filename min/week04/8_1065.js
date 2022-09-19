37c344d22841d172a572e99f0a3772bb0972f863
ALSRUD29 <dev.mnkg@gmail.com>
2022-09-19 19:04:55 +0900

    1068 한수

---
+++ min/week04/8_1065.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = Number(fs.readFileSync('/dev/stdin').toString().trim());
+
+
+    let count = 0;
+    for(let i=1; i <= input; i++){
+        if(i <= 99){
+            count++;
+            continue; 
+        }  
+        let gap1 = Number(String(i)[1])- Number(String(i)[0]);
+        let gap2 = Number(String(i)[2])- Number(String(i)[1]);
+        if(gap1 === gap2){
+            count++;
+        } 
+    }
+    
+console.log(count);

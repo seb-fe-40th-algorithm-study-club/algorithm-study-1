5378a06413fae8c59a28dc1ba9eb0cc5dd0a16e0
billy5982 <kuu0404@naver.com>
2022-09-20 23:02:55 +0900

    1번 11654번 완료

---
+++ mk/week5/1_11654.js
@@ -0,0 +1,11 @@
+//vscode
+const fs = require("fs");
+const input = fs
+  .readFileSync(
+    process.platform === "linux"
+      ? "/dev/stdin"
+      : __filename.split("/").pop().slice(0, -3) + ".txt"
+  )
+  .toString();
+
+console.log(input.charCodeAt());

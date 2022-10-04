4fa90cc3320c5862e502e7f4ae128bb7ea1b4fc1
GitHubJIYEON <jiyeon2954@gmail.com>
2022-10-04 14:23:35 +0900

    6주 2751 완료

---
+++ 이지연_GitHubJIYEON/week6/2_2751.js
@@ -0,0 +1,27 @@
+입력;
+5;
+5;
+4;
+3;
+2;
+1;
+
+출력;
+1;
+2;
+3;
+4;
+5;
+
+let input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .toString()
+  .trim()
+  .split("\n");
+//['5', '5', '4', '3', '2', '1']
+
+input.shift(); //첫째줄 수의 개수 제거
+
+let answer = input.sort((a, b) => a - b); //오름차순 정렬
+
+console.log(answer.join("\n"));

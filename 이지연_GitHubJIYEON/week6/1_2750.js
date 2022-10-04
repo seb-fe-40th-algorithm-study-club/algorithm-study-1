27f27e8ab8e6077e33f9915fcdaff44f6eefc477
GitHubJIYEON <jiyeon2954@gmail.com>
2022-10-04 14:15:09 +0900

    6주 2750 완료

--- 이지연_GitHubJIYEON/week6/1_2750.js
+++ 이지연_GitHubJIYEON/week6/1_2750.js
@@ -1,11 +1,28 @@
-const fs = require("fs");
-const input = fs
-  .readFileSync("/dev/stdin")
+// 입력;
+// 5;
+// 5;
+// 2;
+// 3;
+// 4;
+// 1;
+// 출력;
+// 1;
+// 2;
+// 3;
+// 4;
+// 5;
+
+let input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
   .toString()
   .trim()
-  .split("\n")
-  .map((e) => Number(e));
+  .split("\n");
+//['5', '5', '2', '3', '4', '1']
 
-input.shift();
-input.sort((a, b) => a - b);
-input.forEach((e) => console.log(e));
+input.shift(); //첫째줄 수의 개수 제거
+
+let answer = input.sort((a, b) => a - b); //오름차순 정렬
+
+console.log(answer.join("\n"));
+
+//answer.join() //'1,2,3,4,5'

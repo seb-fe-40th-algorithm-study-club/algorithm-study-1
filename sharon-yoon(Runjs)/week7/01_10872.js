32e52455413173bd0136e7ea5edb3e0c4f571844
sharon-youn <jos9187@gmail.com>
2022-10-13 21:57:56 +0900

    7주차 백준 01_10872번 완료

---
+++ sharon-yoon(Runjs)/week7/01_10872.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const input= fs.readFileSync('dev/stdin').toString().trim().split('\n').map(Number)
+//require('fs').readFileSync('dev/stdin')
+//예제.txt
+
+// console.log(input[0])
+// 팩토리얼 
+// 0! = 1 1! = 1 2! = 1!*2 3! = 2!*3
+function factorial(num) {
+// base
+
+if(num <= 1) return 1;
+
+// recursive
+return num * factorial(num-1)
+}
+
+console.log(factorial(input[0]))

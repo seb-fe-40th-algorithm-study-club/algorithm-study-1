a64d17e0dcbd552ccf2ec4bf6b5dd97f4ac9f3cb
ALSRUD29 <dev.mnkg@gmail.com>
2022-10-23 20:02:38 +0900

    프로그래머스 하샤드수

---
+++ min/week08/7.js
@@ -0,0 +1,9 @@
+//프로그래머스 하샤드 수
+
+function solution(x) {
+    let sum = String(x).split('').map(Number).reduce((a,b)=> a+b);
+    if(x % sum === 0){
+        return true;
+    } 
+    return false;
+}
